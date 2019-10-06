b2edfb94ef7ddcd0a3503bc24b6250dd4d26b787
melissa kobia <melissakobia1997@gmail.com>
2019-10-06 18:23:53 +0300

    Adds a method that gets the key

--- src/main/java/models/Encoding.java
+++ src/main/java/models/Encoding.java
@@ -3,13 +3,20 @@ package models;
 public class Encoding {
 
     private String input;
+    private int key;
+    private String output;
 
     public Encoding(String input,int key, String output) {
         this.input = input;
+        this.key = key;
     }
 
     public String getInput() {
         return this.input;
+    }
+
+    public int getKey() {
+        return this.key;
     }
 
 
