181c1ef0664fc098b1bdf96686e0f49fcbb022c4
melissa kobia <melissakobia1997@gmail.com>
2019-10-06 18:09:51 +0300

    Adds a test that checks the Encoding class the input

--- src/test/java/models/EncodingTest.java
+++ src/test/java/models/EncodingTest.java
@@ -8,8 +8,16 @@ import static org.junit.Assert.assertEquals;
 class EncodingTest {
     @Test
     public void newEncoder_instantiatesCorrectly() {
-        Encoding newEnconcoder = new Encoding("word",3,"output");
-        assertEquals(true, newEnconcoder instanceof Encoding);
+        Encoding newEncoder = new Encoding("word",3,"output");
+        assertEquals(true, newEncoder instanceof Encoding);
 
     }
+
+    @Test
+    public void newEncoder_getsInput() {
+        Encoding newEncoder = new Encoding("word",3,"output");
+        assertEquals("word", newEncoder.getInput());
+    }
+
+
 }
