1b714de6d3e49e1e88f8a29a41add5d7e9780e4b
melissa kobia <melissakobia1997@gmail.com>
2019-10-06 18:22:47 +0300

    Adds a test that checks the Encoding class gets the key

--- src/test/java/models/EncodingTest.java
+++ src/test/java/models/EncodingTest.java
@@ -19,5 +19,15 @@ class EncodingTest {
         assertEquals("word", newEncoder.getInput());
     }
 
+    @Test
+    public void newEncoder_getsKey() {
+        Encoding newEncoder = new Encoding("word",3,"output");
+        assertEquals(3, newEncoder.getKey());
+    }
 
+    @Test
+    public void newEncoder_getsOutput() {
+        Encoding newEncoder = new Encoding("word",3,"output");
+        assertEquals("output", newEncoder.getOutput());
+    }
 }
