aacb00b89490bd3e39c116b0b6e97caa927a743b
melissa kobia <melissakobia1997@gmail.com>
2019-10-06 18:12:58 +0300

    Adds a method that gets the input

--- src/main/java/models/Encoding.java
+++ src/main/java/models/Encoding.java
@@ -1,4 +1,16 @@
 package models;
 
 public class Encoding {
+
+    private String input;
+
+    public Encoding(String input,int key, String output) {
+        this.input = input;
+    }
+
+    public String getInput() {
+        return this.input;
+    }
+
+
 }
