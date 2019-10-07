b2fc7e89077c09c92d082c9dcf42650220ef97f6
melissa kobia <melissakobia1997@gmail.com>
2019-10-07 15:05:17 +0300

    Adds a main function that accepts user inputs and displays the encrypted test

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,42 @@
+import models.Encoding;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class App {
+    public static void main (String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+
+        System.out.println("Enter a text to encrypt: ");
+        String inputString = myScanner.nextLine();
+
+        System.out.println("Enter a key value: ");
+        String keyString = myScanner.nextLine();
+        Integer keyInt = Integer.parseInt(keyString);
+
+        Encoding newEncoder = new Encoding(inputString, keyInt);
+
+        System.out.println("ENCRYPTED TEXT: "+newEncoder.encoder(newEncoder.getInput(),newEncoder.getKey()));
+
+
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
