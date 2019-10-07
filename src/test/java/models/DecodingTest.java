3f9fa889f3cb8271d50e37fd9ae5ed0495b2693b
melissa kobia <melissakobia1997@gmail.com>
2019-10-07 18:25:19 +0300

    Adds a test that checks whether the decoding class gets the input and key

--- src/test/java/models/DecodingTest.java
+++ src/test/java/models/DecodingTest.java
@@ -12,6 +12,18 @@ public class DecodingTest {
 
     }
 
+    @Test
+    public void newDecoder_getsInput() {
+        Decoding newDecoder = new Decoding("word",3);
+        assertEquals("word", newDecoder.getInput());
+    }
+
+    @Test
+    public void newDecoder_getsKey() {
+        Decoding newDecoder = new Decoding("word",3);
+        assertEquals(3, newDecoder.getKey());
+    }
+
 
 
 }
