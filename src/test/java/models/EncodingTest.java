e7dcd3426c0f28c99028ffc203f8e4c00166a7c1
melissa kobia <melissakobia1997@gmail.com>
2019-10-07 18:18:27 +0300

    Adds a test that checks whether the method encrypts a character in uppercase

--- src/test/java/models/EncodingTest.java
+++ src/test/java/models/EncodingTest.java
@@ -5,29 +5,33 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-class EncodingTest {
+public class EncodingTest {
     @Test
     public void newEncoder_instantiatesCorrectly() {
-        Encoding newEncoder = new Encoding("word",3,"output");
+        Encoding newEncoder = new Encoding("word",3);
         assertEquals(true, newEncoder instanceof Encoding);
 
     }
 
     @Test
     public void newEncoder_getsInput() {
-        Encoding newEncoder = new Encoding("word",3,"output");
+        Encoding newEncoder = new Encoding("word",3);
         assertEquals("word", newEncoder.getInput());
     }
 
     @Test
     public void newEncoder_getsKey() {
-        Encoding newEncoder = new Encoding("word",3,"output");
+        Encoding newEncoder = new Encoding("word",3);
         assertEquals(3, newEncoder.getKey());
     }
+    
 
     @Test
-    public void newEncoder_getsOutput() {
-        Encoding newEncoder = new Encoding("word",3,"output");
-        assertEquals("output", newEncoder.getOutput());
+    public void encoder_EncryptsUppercaseCharacterA_D() {
+        Encoding newEncoder = new Encoding("A",3);
+        assertEquals("D", newEncoder.encoder(newEncoder.getInput(),newEncoder.getKey()));
+
     }
+
+
 }
