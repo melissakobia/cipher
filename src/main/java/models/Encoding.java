4a558e0f950a985b65bb4ca1e2d075a80542277b
melissa kobia <melissakobia1997@gmail.com>
2019-10-07 15:02:09 +0300

    Adds a function that encrypts the string

--- src/main/java/models/Encoding.java
+++ src/main/java/models/Encoding.java
@@ -6,9 +6,10 @@ public class Encoding {
     private int key;
     private String output;
 
-    public Encoding(String input,int key, String output) {
+    public Encoding(String input, int key) {
         this.input = input;
         this.key = key;
+
     }
 
     public String getInput() {
@@ -21,5 +22,51 @@ public class Encoding {
 
     public String getOutput() {
         return this.output;
+    }
+
+    public boolean isInputString() {
+        return this.input != null;
+    }
+
+    public String encoder(String input, int key) {
+        Character[] alphabetsUpper = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+        Character[] alphabetsLower = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+
+        int length = input.length();
+
+        //Loops through the l
+        StringBuilder result = new StringBuilder();
+        for (int index = 0; index < length; index++) {
+
+            char letter = input.charAt(index);
+
+            if (Character.isLetter(letter) && Character.isUpperCase(letter)) {
+                for (char alphabet : alphabetsUpper) {
+                    if (alphabet == letter) {
+                        char newAlphabet = (char) ((alphabet + key - 'A') % 26 + 'A');
+                        result.append(newAlphabet);
+
+                    }
+                }
+
+            }
+            else if (Character.isLetter(letter) && Character.isLowerCase(letter)) {
+                for (int i = 0; i < alphabetsLower.length; i++) {
+                    char alphabet = alphabetsLower[i];
+                    if (alphabet == letter) {
+                        char newAlphabet = (char) ((alphabet + key - 'a') % 26 + 'a');
+                        result.append(newAlphabet);
+
+                    }
+                }
+
+            }
+            else if (letter == ' ') {
+                result.append(letter);
+            }
+
+        }
+        return result.toString();
+
     }
 }
