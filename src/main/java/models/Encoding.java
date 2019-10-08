41597d923671de0add22120179d118972296f929
melissa kobia <melissakobia1997@gmail.com>
2019-10-08 08:11:02 +0300

    Final commit

--- src/main/java/models/Encoding.java
+++ src/main/java/models/Encoding.java
@@ -24,9 +24,6 @@ public class Encoding {
         return this.output;
     }
 
-    public boolean isInputString() {
-        return this.input != null;
-    }
 
     public String encoder(String input, int key) {
         Character[] alphabetsUpper = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
